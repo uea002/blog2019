959b5b1c99959c7116854434917faab33a8544f6
Елена Устинникова <elenaustinn@gmail.ru>
2019-10-18 21:42:42 +0300

    Изменения, внесенные на паре 18.10

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  validates :author, presence: true
+  belongs_to :user
+
   validates :body, presence: true
   validates :title, presence: true, length: {maximum: 200}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_172324) do
+ActiveRecord::Schema.define(version: 2019_10_18_180720) do
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_10_18_172324) do
     t.integer "page_views_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_10_18_172324) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end

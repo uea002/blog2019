7a2cb7934390c2f2e83463b5e3b578ccff353216
Alexey Alexandrov <aleksandrov1988@gmail.com>
2019-10-18 20:44:23 +0300

    Добавили пользователей

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_164410) do
+ActiveRecord::Schema.define(version: 2019_10_18_171743) do
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.text "body"
     t.string "title"
     t.integer "page_views_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

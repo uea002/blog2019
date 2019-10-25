6678d961899fe0c262ec3516d2e2457d8e93279f
Alexey ALexandrov <aleksandrov1988@gmail.com>
2019-10-25 14:14:24 +0300

    Docker & db:seed исправлен

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(login: 'admin', admin: true, password: 'Zie4bahj', password_confirmation: 'Zie4bahj')
 100.times do
   Post.create(title: Faker::Lorem.sentence,
-              author: Faker::Name.name,
+              user: user,
               body: Faker::Lorem.paragraph)
 end

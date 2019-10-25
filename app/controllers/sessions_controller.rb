4aade75ea51fa9f445863f7343df2c888549003c
Alexey Alexandrov <aleksandrov1988@gmail.com>
2019-10-25 17:47:08 +0300

    posts#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create    
+  def create
+    @user = User.find_by(login: params[:login])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to posts_path
+      else
+        flash[:notice] = 'Неверный пароль'
+        render :new
+      end
+    else
+      flash[:notice] = 'Пользователь не найден'
+      render :new
+    end
   end
 end

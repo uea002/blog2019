b9a9db6dd7186a0402e824be87f9b20172780e7a
Alexey Alexandrov <aleksandrov1988@gmail.com>
2019-10-25 17:39:24 +0300

    Start login in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new', as: :login
+  post 'sessions', to: 'sessions#create', as: :session
 end

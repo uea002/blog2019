0f2f4538603d7242872d48372d741744e192824d
Елена Устинникова <elenaustinn@gmail.ru>
2019-11-01 16:59:58 +0300

    some

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def load_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
+  post 'sessions', to: 'sessions#create', as: :sessions
 end

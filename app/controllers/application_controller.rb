10e30ba67617d758ddaf52cf4e03d02add0b3bc9
uea002 <elenaustinn@gmail.com>
2019-11-08 14:43:11 +0300

    удалила старый код

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-
-  private
-
-  def load_current_user
-    @current_user = User.find_by(id: session[:user_id])
-  end
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  has_many :posts
-  validates :login, presence: true, uniqueness: true, length: {minimum: 3, maximum: 72}
-end
